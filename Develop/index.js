59bf7df4b404c9138d2f44e3d379d877b723a141
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-08 21:46:55 -0700

    start questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,105 @@
-// array of questions for user
-const questions = [
+// require modules 
+// const fs = require('fs'); 
+const inquirer = require('inquirer'); 
 
-];
+// const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
+const generatePage = require('./utils/generateMarkdown.js');
+
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username? (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false; 
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project name? (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project'
+    }, 
+    {
+        type: '',
+        name: 'license',
+        message: 'What kind of license should your project have?'
+    },
+    {
+        type: '',
+        name: 'install',
+        message: 'What command should be run to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?'
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing to the repo?'
+    }
+]);
+};
+
+questions();
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// } 
+
+//fs.writeFile('./index.html', pageHTML, err => {
+    //if(err) throw new Error (err);
+
+    // success message 
+    // console.log("README complete! Please check the text document");
+// });
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// }
 
 // function call to initialize program
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
+  // license 
 
+  ## Description 
+  ${`description`}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  ${`install`}
+
+  ## Usage 
+  ${`usage`}
+
+  ## License 
+  This project is license under ${`license`}
+
+  ## Contributing 
+  ${`contributors`}
+
+  ## Tests
+  If there are any issues, please run ${`test`}
+
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${`email`}. You can view more of my projects at ${`github`}.
 `;
 }
 
